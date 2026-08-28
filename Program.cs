f999ec5405500fb3be81d61ff8719d10798208bb
BaherKhedr <baherkhedr9@gmail.com>
2026-08-28 21:31:14 +0300

    changed default route in program.cs to showall action and student controller

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace StudentManagementSystemMVC
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Student}/{action=ShowAll}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
