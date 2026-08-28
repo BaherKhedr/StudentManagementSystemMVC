8f3a064c886bf83bb5244ad2bc739b0a71ede20e
BaherKhedr <baherkhedr9@gmail.com>
2026-08-28 21:30:29 +0300

    added error message to regular expression

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,7 @@ namespace StudentManagementSystemMVC.Models
         [Required(ErrorMessage = "Please enter the Student's Id")]
         public int? Id { get; set; }
         [Required(ErrorMessage = "Please enter the Student's Name")]
-        [RegularExpression(@"^[a-zA-Z ]+$")]
+        [RegularExpression(@"^[a-zA-Z ]+$" , ErrorMessage ="Please don't include numbers in the student's Name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter the Student's Age")]
         [Range(10,25 , ErrorMessage = "Student's Age must be between 10 and 25")]
