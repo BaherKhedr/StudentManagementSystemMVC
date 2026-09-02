1e2a4242f12b071cda6902513d832bc6967e5538
BaherKhedr <baherkhedr9@gmail.com>
2026-09-02 18:25:35 +0300

    New Search feature ViewModel

---
+++ ViewModels/StudentSearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagementSystemMVC.ViewModels
+{
+    public class StudentSearchViewModel
+    {
+        public string? Name { get; set; }
+        public int? AgeFrom { get; set; }
+        public int? AgeTo { get; set; }
+        public double? GradeFrom { get; set; }
+        public double? GradeTo { get; set; }
+        public string? SortBy { get; set; }
+    }
+}
